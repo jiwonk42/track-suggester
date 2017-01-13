995b79b84512f30f8b93546432c041e3f5099952
Jiwon Kang <jiwonk42@gmail.com>
2017-01-13 11:47:37 -0800

    add a div for result at the end of the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
-    $("").hide();
+    //$("").hide();
 
+    var name = $("input#name").val();
+    var experience = $("select#experience").val();
+    var interest = $("input:radio[name=interest]:checked").val();
+    var language = $("input:radio[name=language]:checked").val();
+    console.log(name);
+    console.log(experience);
+    console.log(interest);
+    console.log(language);
+
+    if(name) {
+      
+    } else {
+      alert("Please enter your name.");
+    }
     event.preventDefault();
   });
 });
